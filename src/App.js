4be8d01797fdbbabb3050799691f865f9671bac6
Ernesto Mancebo <ernesmancebo@gmail.com>
2022-05-22 12:20:39 -0400

    Adds react code

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+          this has been changed
+
+          learn react
+      </header>
+    </div>
+  );
+}
+
+export default App;
